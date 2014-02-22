fd5a4df3ca4273b608c49670f3cf2a4e543feec7
ShadowMen <klaus.henne1@web.de>
2014-02-23 00:53:02 +0100

    Leere Game Klasse hinzugefügt

---
+++ Win_Tetris/Win_Tetris/Game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Win_Tetris
+{
+    class Game
+    {
+        //Objekte
+
+        //Methoden
+        public void run()
+        {
+        }
+
+        public void pause()
+        {
+        }
+
+        public void update()
+        {
+        }
+
+        public void handleInput()
+        {
+        }
+
+        public void draw(Graphics gfx)
+        {
+        }
+    }
+}
