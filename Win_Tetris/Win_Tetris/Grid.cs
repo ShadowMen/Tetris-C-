dd3327c0a1c6ec4fee0585ee3b8e25b4290b3659
ShadowMen <klaus.henne1@web.de>
2014-02-26 08:06:51 +0100

    "Fliegender Block" hinzugefügt

--- Win_Tetris/Win_Tetris/Grid.cs
+++ Win_Tetris/Win_Tetris/Grid.cs
@@ -9,13 +9,20 @@ namespace Win_Tetris
     class Grid
     {
         //Variabeln
-        int[,] field = new int[10, 22];
+        int[,] field = new int[22, 10];
+        Block flyingBlock = new TBlock();
         int blockSize = 10;
 
         //Properties
         public int[,] Field
         {
             get { return field; }
+        }
+
+        public Block FlyingBlock
+        {
+            get { return flyingBlock; }
+            set { flyingBlock = value; }
         }
 
         public int BlockSize
@@ -27,13 +34,16 @@ namespace Win_Tetris
         //Methoden
         public void draw(Graphics gfx)
         {
-            Pen pen = new Pen(Color.Black);
+            Pen pen = new Pen(Color.LightGray);
             for (int y = 0; y < 22; y++)
             {
                 for (int x = 0; x < 10; x++)
                 {
-                    switch (field[x, y])
+                    switch (field[y, x])
                     {
+                        case 0:
+                            pen.Color = Color.LightGray;
+                            break;
                         case 1:
                             pen.Color = Color.Blue;
                             break;
@@ -43,6 +53,27 @@ namespace Win_Tetris
                     }
 
                     gfx.FillRectangle(pen.Brush, x * blockSize + 1, y * blockSize + 1, blockSize - 1, blockSize - 1);
+                }
+            }
+
+            for (int y = 0; y < flyingBlock.Size; y++)
+            {
+                for (int x = 0; x < flyingBlock.Size; x++)
+                {
+                    switch (flyingBlock.Blocks[y, x])
+                    {
+                        case 0:
+                            pen.Color = Color.LightGray;
+                            break;
+                        case 1:
+                            pen.Color = Color.Blue;
+                            break;
+                        case 2:
+                            pen.Color = Color.Red;
+                            break;
+                    }
+
+                    gfx.FillRectangle(pen.Brush, (x + flyingBlock.PositionX) * blockSize + 1, (y + flyingBlock.PositionY) * blockSize + 1, blockSize - 1, blockSize - 1);
                 }
             }
         }
